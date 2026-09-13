a5982ca2103bb496d54be3339fb1e30fc4c5fa87
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2026-09-13 12:03:46 +0300

    refactoring

--- src/pyformula/formula.py
+++ src/pyformula/formula.py
@@ -1,35 +1,7 @@
-import operator
 from collections.abc import Callable
-from decimal import Decimal
-from typing import cast
 
-from .models import NumberType, OperatorFn, OperatorType
-
-
-def _safe_operate(fn: OperatorFn) -> OperatorFn:
-    def inner(a: NumberType, b: NumberType) -> NumberType:
-        if isinstance(a, Decimal) and isinstance(b, float):
-            b = Decimal.from_float(b)
-
-        if isinstance(b, Decimal) and isinstance(a, float):
-            a = Decimal.from_float(a)
-
-        return fn(a, b)
-
-    return inner
-
-
-OPERATORS: dict[OperatorType, OperatorFn] = {
-    "add": _safe_operate(cast("OperatorFn", operator.add)),
-    "subtract": _safe_operate(cast("OperatorFn", operator.sub)),
-    "multiply": _safe_operate(cast("OperatorFn", operator.mul)),
-    "divide": _safe_operate(operator.truediv),
-    "floor_divide": _safe_operate(operator.floordiv),
-    "modulo": _safe_operate(cast("OperatorFn", operator.mod)),
-    "power": _safe_operate(operator.pow),
-    "left_shift": _safe_operate(operator.lshift),
-    "right_shift": _safe_operate(operator.rshift),
-}
+from .models import NumberType, OperatorType
+from .operators import OPERATORS_SAFE_FUNCTIONS
 
 
 class Formula[T]:
@@ -100,7 +72,7 @@ class Formula[T]:
         *,
         reverse: bool,
     ) -> "Formula[T]":
-        operator_fn = OPERATORS[operator]
+        operator_fn = OPERATORS_SAFE_FUNCTIONS[operator]
         other_fn = other if isinstance(other, Formula) else Formula(lambda _: other)
 
         def inner(obj: T) -> NumberType:
