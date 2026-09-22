f40e1c89f5fe1960f2ef72eb5f9cbe9d7f38e1d9
aaltatan <a.altatan@gmail.com>
2026-09-22 05:51:42 +0300

    renaming

--- src/pyformula/math.py
+++ src/pyformula/math.py
@@ -1,4 +1,4 @@
-import math as builtin_math
+import math
 from collections.abc import Callable
 
 from .formula import Formula
@@ -9,15 +9,15 @@ from .formula import Formula
 
 
 def ceil[T](fm: Formula[T], /) -> Formula[T]:
-    return _wrap(fm, math_fn=builtin_math.ceil)
+    return _wrap(fm, math_fn=math.ceil)
 
 
 def floor[T](fm: Formula[T], /) -> Formula[T]:
-    return _wrap(fm, math_fn=builtin_math.floor)
+    return _wrap(fm, math_fn=math.floor)
 
 
 def trunc[T](fm: Formula[T], /) -> Formula[T]:
-    return _wrap(fm, math_fn=builtin_math.trunc)
+    return _wrap(fm, math_fn=math.trunc)
 
 
 # ----------------------------------------------
@@ -27,12 +27,12 @@ def trunc[T](fm: Formula[T], /) -> Formula[T]:
 
 def sqrt[T](fm: Formula[T], /) -> Formula[T]:
     """Return the square root of formula."""
-    return _wrap(fm, math_fn=builtin_math.sqrt)
+    return _wrap(fm, math_fn=math.sqrt)
 
 
 def cbrt[T](fm: Formula[T], /) -> Formula[T]:
     """Return the cube root of formula."""
-    return _wrap(fm, math_fn=builtin_math.cbrt)
+    return _wrap(fm, math_fn=math.cbrt)
 
 
 # ----------------------------------------------
@@ -42,12 +42,12 @@ def cbrt[T](fm: Formula[T], /) -> Formula[T]:
 
 def exp[T](fm: Formula[T], /) -> Formula[T]:
     """Return e raised to the power of formula."""
-    return _wrap(fm, math_fn=builtin_math.exp)
+    return _wrap(fm, math_fn=math.exp)
 
 
 def exp2[T](fm: Formula[T], /) -> Formula[T]:
     """Return 2 raised to the power of formula."""
-    return _wrap(fm, math_fn=builtin_math.exp2)
+    return _wrap(fm, math_fn=math.exp2)
 
 
 def expm1[T](fm: Formula[T], /) -> Formula[T]:
@@ -55,12 +55,12 @@ def expm1[T](fm: Formula[T], /) -> Formula[T]:
 
     This function avoids the loss of precision involved in the direct evaluation of exp(formula)-1.
     """
-    return _wrap(fm, math_fn=builtin_math.expm1)
+    return _wrap(fm, math_fn=math.expm1)
 
 
 def log10[T](fm: Formula[T], /) -> Formula[T]:
     """Return the base 10 logarithm of formula."""
-    return _wrap(fm, math_fn=builtin_math.log10)
+    return _wrap(fm, math_fn=math.log10)
 
 
 def log1p[T](fm: Formula[T], /) -> Formula[T]:
@@ -68,12 +68,12 @@ def log1p[T](fm: Formula[T], /) -> Formula[T]:
 
     The result is computed in a way which is accurate for formula near zero.
     """
-    return _wrap(fm, math_fn=builtin_math.log1p)
+    return _wrap(fm, math_fn=math.log1p)
 
 
 def log2[T](fm: Formula[T], /) -> Formula[T]:
     """Return the base 2 logarithm of formula."""
-    return _wrap(fm, math_fn=builtin_math.log2)
+    return _wrap(fm, math_fn=math.log2)
 
 
 # ----------------------------------------------
@@ -83,62 +83,62 @@ def log2[T](fm: Formula[T], /) -> Formula[T]:
 
 def sin[T](fm: Formula[T], /) -> Formula[T]:
     """Return the sine of formula (measured in radians)."""
-    return _wrap(fm, math_fn=builtin_math.sin)
+    return _wrap(fm, math_fn=math.sin)
 
 
 def sinh[T](fm: Formula[T], /) -> Formula[T]:
     """Return the hyperbolic sine of formula."""
-    return _wrap(fm, math_fn=builtin_math.sinh)
+    return _wrap(fm, math_fn=math.sinh)
 
 
 def asin[T](fm: Formula[T], /) -> Formula[T]:
     """Return the arc sine (measured in radians) of formula."""
-    return _wrap(fm, math_fn=builtin_math.asin)
+    return _wrap(fm, math_fn=math.asin)
 
 
 def asinh[T](fm: Formula[T], /) -> Formula[T]:
     """Return the inverse hyperbolic sine of formula."""
-    return _wrap(fm, math_fn=builtin_math.asinh)
+    return _wrap(fm, math_fn=math.asinh)
 
 
 def cos[T](fm: Formula[T], /) -> Formula[T]:
     """Return the cosine of formula (measured in radians)."""
-    return _wrap(fm, math_fn=builtin_math.cos)
+    return _wrap(fm, math_fn=math.cos)
 
 
 def cosh[T](fm: Formula[T], /) -> Formula[T]:
     """Return the hyperbolic cosine of formula."""
-    return _wrap(fm, math_fn=builtin_math.cosh)
+    return _wrap(fm, math_fn=math.cosh)
 
 
 def acos[T](fm: Formula[T], /) -> Formula[T]:
     """Return the arc cosine (measured in radians) of formula."""
-    return _wrap(fm, math_fn=builtin_math.acos)
+    return _wrap(fm, math_fn=math.acos)
 
 
 def acosh[T](fm: Formula[T], /) -> Formula[T]:
     """Return the hyperbolic arc cosine of formula."""
-    return _wrap(fm, math_fn=builtin_math.acosh)
+    return _wrap(fm, math_fn=math.acosh)
 
 
 def tan[T](fm: Formula[T], /) -> Formula[T]:
     """Return the tangent of formula (measured in radians)."""
-    return _wrap(fm, math_fn=builtin_math.tan)
+    return _wrap(fm, math_fn=math.tan)
 
 
 def tanh[T](fm: Formula[T], /) -> Formula[T]:
     """Return the hyperbolic tangent of formula."""
-    return _wrap(fm, math_fn=builtin_math.tanh)
+    return _wrap(fm, math_fn=math.tanh)
 
 
 def atan[T](fm: Formula[T], /) -> Formula[T]:
     """Return the arc tangent (measured in radians) of formula."""
-    return _wrap(fm, math_fn=builtin_math.atan)
+    return _wrap(fm, math_fn=math.atan)
 
 
 def atanh[T](fm: Formula[T], /) -> Formula[T]:
     """Return the hyperbolic arc tangent of formula."""
-    return _wrap(fm, math_fn=builtin_math.atanh)
+    return _wrap(fm, math_fn=math.atanh)
 
 
 # ----------------------------------------------
@@ -148,12 +148,12 @@ def atanh[T](fm: Formula[T], /) -> Formula[T]:
 
 def degrees[T](fm: Formula[T], /) -> Formula[T]:
     """Convert angle formula from radians to degrees."""
-    return _wrap(fm, math_fn=builtin_math.degrees)
+    return _wrap(fm, math_fn=math.degrees)
 
 
 def radians[T](fm: Formula[T], /) -> Formula[T]:
     """Convert angle formula from degrees to radians."""
-    return _wrap(fm, math_fn=builtin_math.radians)
+    return _wrap(fm, math_fn=math.radians)
 
 
 # ----------------------------------------------
@@ -163,22 +163,22 @@ def radians[T](fm: Formula[T], /) -> Formula[T]:
 
 def erf[T](fm: Formula[T], /) -> Formula[T]:
     """Return the error function of formula."""
-    return _wrap(fm, math_fn=builtin_math.erf)
+    return _wrap(fm, math_fn=math.erf)
 
 
 def erfc[T](fm: Formula[T], /) -> Formula[T]:
     """Complementary error function."""
-    return _wrap(fm, math_fn=builtin_math.erfc)
+    return _wrap(fm, math_fn=math.erfc)
 
 
 def gamma[T](fm: Formula[T], /) -> Formula[T]:
     """Return the gamma function of formula."""
-    return _wrap(fm, math_fn=builtin_math.gamma)
+    return _wrap(fm, math_fn=math.gamma)
 
 
 def lgamma[T](fm: Formula[T], /) -> Formula[T]:
     """Return the natural logarithm of the gamma function of formula."""
-    return _wrap(fm, math_fn=builtin_math.lgamma)
+    return _wrap(fm, math_fn=math.lgamma)
 
 
 # ----------------------------------------------
@@ -188,12 +188,12 @@ def lgamma[T](fm: Formula[T], /) -> Formula[T]:
 
 def fabs[T](fm: Formula[T], /) -> Formula[T]:
     """Return the absolute value of formula."""
-    return _wrap(fm, math_fn=builtin_math.fabs)
+    return _wrap(fm, math_fn=math.fabs)
 
 
 def ulp[T](fm: Formula[T], /) -> Formula[T]:
     """Return the distance between formula and the next larger representable number."""
-    return _wrap(fm, math_fn=builtin_math.ulp)
+    return _wrap(fm, math_fn=math.ulp)
 
 
 # ----------------------------------------------
